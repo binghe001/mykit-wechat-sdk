09072aaf26a8124e1eb166b714d14ca1fd28a512
liuyazhuang <1028386804@qq.com>
2018-07-30 15:00:45 +0800

    路由规则细化

--- mykit-wechat-beans/src/main/java/io/mykit/wechat/mp/beans/router/receive/WxReceiveRouterResult.java
+++ mykit-wechat-beans/src/main/java/io/mykit/wechat/mp/beans/router/receive/WxReceiveRouterResult.java
@@ -8,9 +8,28 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
+ *
  * @Author: liuyazhuang
  * @Date: 2018/7/30 10:29
  * @Description: 微信路由转化的JavaBean结果,实际业务根据routerType中的类型将baseReceiveMessage强制转化为具体的JavaBean
+ *
+ * 具体转化规则如下:
+ * routerType类型                                      对应的常量                                               WxBaseReceiveMessage强制转化的具体类型
+ * router_text                                    WxConstants.ROUTER_TEXT                                              WxReceiveTextMessage
+ * router_image                                   WxConstants.ROUTER_IMAGE                                             WxReceiveImageMessage
+ * router_voice                                   WxConstants.ROUTER_VOICE                                             WxReceiveVoiceMessage
+ * router_video                                   WxConstants.ROUTER_VIDEO                                             WxReceiveVideoMessage
+ * router_shortvideo                              WxConstants.ROUTER_SHORTVIDEO                                        WxReceiveShortVideoMessage
+ * router_location                                WxConstants.ROUTER_LOCATION                                          WxReceiveLocationMessage
+ * router_link                                    WxConstants.ROUTER_LINK                                              WxReceiveLinkMessage
+ * router_event_subscribe                         WxConstants.ROUTER_EVENT_SUBSCRIBE                                   WxSubscribeEventMessage
+ * router_event_qrcode_subscribe                  WxConstants.ROUTER_EVENT_QRCODE_SUBSCRIBE                            WxQrcodeSubscribeEventMessage
+ * router_event_unsubscribe                       WxConstants.ROUTER_EVENT_UNSUBSCRIBE                                 WxUnSubscribeEventMessage
+ * router_event_scan                              WxConstants.ROUTER_EVENT_SCAN                                        WxQrcodeScanEventMessage
+ * router_event_location                          WxConstants.ROUTER_EVENT_LOCATION                                    WxLocationEventMessage
+ * router_event_click                             WxConstants.ROUTER_EVENT_CLICK                                       WxMenuClientEventMessage
+ * router_event_view                              WxConstants.ROUTER_EVENT_VIEW                                        WxMenuPushEventMessage
+ *
  */
 @Data
 @NoArgsConstructor
--- mykit-wechat-test/src/main/java/io/mykit/wechat/router/WxReceiveMessageRouterTest.java
+++ mykit-wechat-test/src/main/java/io/mykit/wechat/router/WxReceiveMessageRouterTest.java
@@ -1,11 +1,17 @@
 package io.mykit.wechat.router;
 
 import io.mykit.wechat.mp.beans.router.receive.WxReceiveRouterResult;
+import io.mykit.wechat.mp.beans.xml.receive.base.WxBaseReceiveMessage;
+import io.mykit.wechat.mp.beans.xml.receive.image.WxReceiveImageMessage;
 import io.mykit.wechat.mp.beans.xml.receive.text.WxReceiveTextMessage;
 import io.mykit.wechat.mp.core.router.WxReceiveMessageRouter;
 import io.mykit.wechat.utils.constants.WxConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: liuyazhuang
@@ -15,8 +21,16 @@ import org.junit.Test;
 @Slf4j
 public class WxReceiveMessageRouterTest {
 
+    private Map<String, Class<? extends WxBaseReceiveMessage>> map = new HashMap<String, Class<? extends WxBaseReceiveMessage>>();
+
+    @Before
+    public void init(){
+        map.put(WxConstants.ROUTER_TEXT, WxReceiveTextMessage.class);
+    }
+
     @Test
-    public void testRouter(){
+    public void testTextRouter() throws Exception {
+        log.info(map.toString());
         long startTime = System.currentTimeMillis();
         log.info("开始：" + startTime);
         String str = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[fromUser]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[this is a test]]></Content><MsgId>1234567890123456</MsgId></xml>";
@@ -24,9 +38,37 @@ public class WxReceiveMessageRouterTest {
         if(WxConstants.ROUTER_TEXT.equals(wxReceiveRouterResult.getRouterType())){
             log.info(wxReceiveRouterResult.toJsonString());
             log.info(wxReceiveRouterResult.getBaseReceiveMessage().toJsonString());
+            log.info(String.valueOf(wxReceiveRouterResult.getBaseReceiveMessage() instanceof  WxReceiveTextMessage));
             WxReceiveTextMessage wxReceiveTextMessage = (WxReceiveTextMessage) wxReceiveRouterResult.getBaseReceiveMessage();
             log.info(wxReceiveTextMessage.toString());
         }
+        long endTime = System.currentTimeMillis();
+        log.info("结束：" + endTime);
+        log.info("间隔：" + (endTime - startTime));
+    }
+    @Test
+    public void testImageRouter(){
+        long startTime = System.currentTimeMillis();
+        log.info("开始：" + startTime);
+        String str = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[fromUser]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[image]]></MsgType><PicUrl><![CDATA[this is a url]]></PicUrl><MediaId><![CDATA[media_id]]></MediaId><MsgId>1234567890123456</MsgId></xml>";
+        WxReceiveRouterResult wxReceiveRouterResult = WxReceiveMessageRouter.router(str);
+        if(WxConstants.ROUTER_IMAGE.equals(wxReceiveRouterResult.getRouterType())){
+            log.info(wxReceiveRouterResult.toJsonString());
+            log.info(wxReceiveRouterResult.getBaseReceiveMessage().toJsonString());
+            WxReceiveImageMessage wxReceiveImageMessage = (WxReceiveImageMessage) wxReceiveRouterResult.getBaseReceiveMessage();
+            log.info(wxReceiveImageMessage.toString());
+        }
+        long endTime = System.currentTimeMillis();
+        log.info("结束：" + endTime);
+        log.info("间隔：" + (endTime - startTime));
+    }
+    @Test
+    public void testScanSubscriveRouter(){
+        long startTime = System.currentTimeMillis();
+        log.info("开始：" + startTime);
+        String str = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[FromUser]]></FromUserName><CreateTime>123456789</CreateTime><MsgType><![CDATA[event]]></MsgType><Event><![CDATA[subscribe]]></Event><EventKey><![CDATA[qrscene_123123]]></EventKey><Ticket><![CDATA[TICKET]]></Ticket></xml>";
+        WxReceiveRouterResult wxReceiveRouterResult = WxReceiveMessageRouter.router(str);
+        log.info(wxReceiveRouterResult.toJsonString());
         long endTime = System.currentTimeMillis();
         log.info("结束：" + endTime);
         log.info("间隔：" + (endTime - startTime));
